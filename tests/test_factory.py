21d6a3a2ae360cdf0d0b728227d3da271d10ca57
Abhinandan Jain <nandanjain04@gmail.com>
2025-10-25 21:20:38 +0530

    merge fix

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import unittest
-from server import create_application
-
-class TestApplication(unittest.TestCase):
-	"""TestApplication verifies the application's basic startup behavior.
-
-	Asserts that a
-	GET request to the "/initial" endpoint returns the expected JSON payload
-	{"message": "hello world"}.
-	"""
-
-	def setUp(self):
-		"""Setup before each testcase"""
-		self.application = create_application({ "TESTING": True }).test_client()
-
-	def tearDown(self):
-		"""Tear down after each testcase"""
-		del self.application
-
-	def test_initial(self):
-		"""Test for initial code"""
-		self.assertEqual(self.application.get("/initial").get_json(), {"message": "hello world"})
-=======
 from tests.server_base import BaseTestCase
 
 class TestFactory(BaseTestCase):
@@ -31,4 +7,3 @@ class TestFactory(BaseTestCase):
 			response = self.application.get("/initial")
 			self.assertEqual(response.status_code, 200)
 			self.assertEqual(response.get_json(), {"message": "hello world"})
->>>>>>> a0f3822b470549830a85d546992eeca23628b250
